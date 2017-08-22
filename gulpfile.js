3ec92693a4e781c59bddae03cfdb15f322417914
hemansin <hemansin>
2017-08-22 12:32:02 +0530

    modular gulp, uncss added

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,18 @@
 var gulp = require('gulp');
-var template = require('gulp-template');
 var server = require('gulp-server-livereload');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var fs = require('fs');
-var gutil = require('gulp-util');
-var notify = require("gulp-notify");
-var autoprefixer = require('gulp-autoprefixer');
-
-gulp.task('styles', function() {
-  gulp.src('./scss/main.scss')
-    .pipe(sass({
-        outputStyle: 'expanded', // nested, expanded, compact, compressed, 
-        imagePath: '../images',
-        precision: 3,
-        sourceComments: 'map',
-        errLogToConsole: true
-    }))
-    .on('error', notify.onError(function (error) {
-        return 'An error occurred while compiling sass.\nLook in the console for details.\n' + error;
-    }))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('./app/css/'))
-    .pipe(notify({
-        message: "Compilation Successful"
-    }));
-});
-
-gulp.task('templates', function () {
-  var templates = {};
-  var files = fs.readdirSync('./pages/partials')
-      .filter(function(file) {
-        // return filename
-        if (file.charAt(0) === '_') {
-          return file;
-        }
-
-      })
-      // add them to the templates object
-      .forEach(function(template) {
-        var slug = template.replace('_', '').replace('.html', '');
-        templates[slug] = fs.readFileSync("./pages/partials/" + template, "utf8");
-      });
-
-  return gulp.src(['./pages/*.html'])
-      .pipe(template(templates))
-      .on('error', gutil.log)
-      .pipe(gulp.dest('./app'));
-});
+var styles = require('./gulp-tasks/sass')();
+var templates = require('./gulp-tasks/templates')();
+var scripts = require('./gulp-tasks/scripts')();
+var copy = require('./gulp-tasks/copy')();
+var config = require('./gulp-tasks/config');
 
 // Watch task
-gulp.task('default',function() {
+gulp.task('default', ['purifycss', 'templates', 'scripts', 'copy'], function() {
   // run task initially, after that watch
-  gulp.start(['styles', 'templates']);
-  gulp.watch('./scss/**/*.scss',['styles']);
-  gulp.watch('pages/**/*.html',['templates']);
-  gulp.src('./app')
+  // gulp.start(['styles', 'templates']);
+  gulp.watch(config.source + 'scss/**/*.scss',['styles']);
+  gulp.watch(config.source + 'pages/**/*.html',['templates']);
+  gulp.src(config.dest)
     .pipe(server({
       livereload: true,
       directoryListing: false,
